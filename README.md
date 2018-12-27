864ebf415075d276762db564218999a5e7a2141a
PhrantiK <harsley@gmail.com>
2018-12-27 21:48:22 +0800

    1.1 Update - use existing .env

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# PhrantiK/helpscout-invoiceninja
+# PhrantiK/helpscout-invoiceninja 1.1
 
 Dynamic app for [Help Scout](http://helpscout.net) that fetches invoices from InvoiceNinja.
 
@@ -6,16 +6,15 @@ Finds all invoices associated with the customer email(s).
 
 ## Install
 
-1. Git clone or download onto your webhost
-2. Run: `composer install` to install dependencies
+1. Clone into a folder from your InvoiceNinja public folder:
+   git clone https://github.com/PhrantiK/helpscout-invoiceninja.git choosefoldername
+2. Run: `composer update` to install dependencies
 3. Generate random secret key: `date | md5sum`
 4. Put secret key in index.php
-5. Put InvoiceNinja database credentials in index.php
-6. Change locale if needed (note: only tested with en-US)
-6. Change domain to your domain
-7. Set up a custom app in Help Scout
+5. Change locale if needed (note: only tested with en-US)
+6. Set up the custom app in Help Scout with your URL & Secret key
 
 ## todo
 
 - [ ] Create customer summary
-- [ ] Update to use InvoiceNinja .env file
+- [x] Update to use InvoiceNinja .env file
--- index.php
+++ index.php
@@ -2,28 +2,33 @@
 
 require 'vendor/autoload.php';
 
+$dotenv = new Dotenv\Dotenv(__DIR__ . '/../../');
+$dotenv->load();
+
 use HelpScoutApp\DynamicApp;
 use Illuminate\Database\Eloquent\Model as Model;
 use Carbon\Carbon;
 
 setlocale(LC_MONETARY, 'en_US.UTF-8');
 
-$app = new DynamicApp('YOURSECRETKEYGOESHERE');
+$app = new DynamicApp('YOURSECRETKEYHERE');
 if ($app->isSignatureValid())
 {
   $customer = $app->getCustomer();
 
   $settings = array(
-    'driver'    => 'mysql',
-    'host'      => 'localhost',
-    'port'      => '3306',
-    'database'  => 'ninja',
-    'username'  => 'ninja',
-    'password'  => 'ninja',
+    'driver'    => getenv('DB_TYPE'),
+    'host'      => getenv('DB_HOST'),
+    'port'      => '3306'
+    'database'  => getenv('DB_DATABASE'),
+    'username'  => getenv('DB_USERNAME'),
+    'password'  => getenv('DB_PASSWORD'),
     'charset'   => 'utf8mb4',
     'collation' => 'utf8mb4_unicode_ci',
     'prefix'    => '',
   );
+
+  $hostname = getenv('APP_URL');
 
   $connFactory = new \Illuminate\Database\Connectors\ConnectionFactory(new Illuminate\Container\Container);
   $conn = $connFactory->make($settings);
@@ -37,24 +42,21 @@ if ($app->isSignatureValid())
       public $timestamps = false;
 
       public function invoice_status()
-      {
+    {
         return $this->belongsTo('invoice_status');
-      }
+    }
 
-      protected $dates = ['created_at'];
-
+    protected $dates = ['created_at'];
   }
 
   class Invoice_status extends Model {
 
       public $timestamps = false;
-
   }
 
   class Contact extends Model {
 
     public $timestamps = false;
-
   }
 
   $contacts = Contact::whereIn('email', $customer->getEmails())->pluck('client_id');
@@ -62,12 +64,12 @@ if ($app->isSignatureValid())
   $invoices = Invoice::whereIn('client_id', $contacts)->with('invoice_status')->latest()->take(10)->get();
 
   $statuscolor = collect([
-    ['id' => '1', 'color' => 'cancelled'],  #RED
-    ['id' => '2', 'color' => 'cancelled'],  #RED
-    ['id' => '3', 'color' => 'open'],       #ORANGE
-    ['id' => '4', 'color' => 'open'],       #ORANGE
-    ['id' => '5', 'color' => 'completed'],  #GREEN
-    ['id' => '6', 'color' => 'completed'],  #GREEN
+    ['id' => '1', 'color' => 'cancelled'],
+    ['id' => '2', 'color' => 'cancelled'],
+    ['id' => '3', 'color' => 'open'],
+    ['id' => '4', 'color' => 'open'],
+    ['id' => '5', 'color' => 'completed'],
+    ['id' => '6', 'color' => 'completed'],
   ]);
 
   $html = array();
@@ -79,7 +81,7 @@ if ($app->isSignatureValid())
       $html[] = '<table class="table-condensed ecomm-app" style="width:100%;">';
       $html[] = '<tbody>';
       $html[] = '<tr>';
-      $html[] = '<td class="num"><a href="https://your.invoiceninja.domain/invoices/'.$invoice->id.'/edit" target="_blank">'.$invoice->invoice_number.'</a></td>';
+      $html[] = '<td class="num"><a href="'.$hostname.'/invoices/'.$invoice->id.'/edit" target="_blank" alt="Paid" title="Paid">'.$invoice->invoice_number.'</a></td>';
       $html[] = '<td style="text-align:right;">'.money_format('%.2n', $invoice->amount).'</td>';
       $html[] = '</tr>';
       $html[] = '<tr class="order-info">';
